34fbe0b68333a5f92468ba5f3c90fc9620a5a304
vishal kumar <vishalsaw235@gmail.com>
2025-05-28 01:58:20 +0530

    Add Cloudinary file upload utility and Multer config for file handling

---
+++ src/middileware/multer.middileware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/tep");
+  },
+  filename: function (req, file, cb) {
+    //Todo for me
+
+    //const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({
+  storage,
+});
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+//Configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary. File srs:" + response.url);
+
+    // Once the file is uploaded, we would like to delete it from our server
+
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
